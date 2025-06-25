a6c2226cee4d74c066867d4116a9a2133d8d5938
FrancoisToureille <toureillefrancois@gmail.com>
2025-06-25 20:28:01 +0200

    test lsa

--- types.h
+++ types.h
@@ -1,81 +1,77 @@
-#pragma once
+#ifndef TYPES_H
+#define TYPES_H
 
-#include <pthread.h>   // ✅ Pour pthread_mutex_t
-#include <time.h>      // ✅ Pour time_t
+#include <pthread.h>
 
-// Constantes
-#define PORT_DIFFUSION 9191
-#define TAILLE_BUFFER 2048
-#define IP_DIFFUSION "10.1.0.255"
-#define NB_MAX_VOISINS 10
-#define NB_MAX_INTERFACES 5
-#define INTERVALLE_HELLO 20
-#define DELAI_EXPIRATION_VOISIN 30
-#define NB_MAX_ROUTES 50
-
-// Structures
-typedef struct {
-    char id_routeur[32];
-    char adresse_ip[16];
-    char interface[16];
-    int metrique;
-    time_t dernier_hello;
-    int bande_passante_mbps;
-    int etat_lien;
-} voisin_t;
+#define NB_MAX_VOISINS 100
+#define NB_MAX_INTERFACES 10
+#define NB_MAX_ROUTES 100
+#define NB_MAX_TOPOLOGY 100
+#define TAILLE_BUFFER 1024
+#define INTERVALLE_HELLO 5
+#define PORT_DIFFUSION 5000
 
 typedef struct {
-    char nom[16];
-    char adresse_ip[16];
-    char ip_diffusion[16];
-    int actif;
+    char nom[32];
+    char ip_locale[32];
+    char ip_diffusion[32];
+    int active;
 } interface_reseau_t;
 
 typedef struct {
-    char destination[16];
-    char prochain_saut[16];
-    char interface[16];
-    int metrique;
+    char router_id[32];
+    char ip_address[32];
+    char interface[32];
+    int metric;
+    int bandwidth_mbps;
+    int link_state;
+    time_t dernier_hello;
+} voisin_t;
+
+typedef struct {
+    char destination[32];
+    char next_hop[32];
+    char interface[32];
+    int metric;
     int nombre_sauts;
-    int bande_passante;
+    int debit;
 } route_t;
 
 typedef struct {
-    char id_routeur[32];
-    int numero_sequence;
-    time_t horodatage;
+    char router_id[32];
+    int timestamp;
     int nb_liens;
     voisin_t liens[NB_MAX_VOISINS];
 } lsa_t;
 
 typedef struct {
-    char id_routeur[32];
+    char router_id[32];
+    int visited;
     int distance;
-    char prochain_saut[16];
-    char interface[16];
-    int visite;
-    int bande_passante;
-} noeud_dijkstra_t;
+    char next_hop[32];
+    char interface[32];
+    int debit;
+} dijkstra_node_t;
 
-// Variables globales (extern)
+// Déclarations globales (extern si utilisées dans plusieurs fichiers)
 extern voisin_t voisins[NB_MAX_VOISINS];
 extern int nombre_voisins;
 extern pthread_mutex_t mutex_voisins;
 
 extern interface_reseau_t interfaces[NB_MAX_INTERFACES];
 extern int nombre_interfaces;
+extern pthread_mutex_t mutex_interfaces;
 
 extern route_t table_routage[NB_MAX_ROUTES];
 extern int nombre_routes;
 extern pthread_mutex_t mutex_routage;
 
-extern lsa_t base_topologie[NB_MAX_VOISINS];
-extern int taille_base_topologie;
+extern lsa_t base_topologie[NB_MAX_TOPOLOGY];
+extern int taille_topologie;
 extern pthread_mutex_t mutex_topologie;
-
-extern noeud_dijkstra_t noeuds[NB_MAX_VOISINS];
-extern int nombre_noeuds;
 
 extern int socket_diffusion;
 extern int socket_ecoute;
-extern volatile int en_fonctionnement;
+extern int en_fonctionnement;
+
+#endif
